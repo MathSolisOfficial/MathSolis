134f16664a1fcdbf44067fbaba9100c26a14f148
MathSolisOfficial <mathsolisofficial@gmail.com>
2026-02-22 12:55:32 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdown = document.querySelector(".dropdown");
+    const toggle = dropdown.querySelector(".dropdown-toggle");
+    const content = dropdown.querySelector(".dropdown-content");
+
+    // клик по кнопке Teorija
+    toggle.addEventListener("click", function(e) {
+        e.preventDefault();
+        dropdown.classList.add("active");
+    });
+
+    // закрытие при клике вне блока
+    document.addEventListener("click", function(e) {
+        if (!dropdown.contains(e.target)) {
+            dropdown.classList.remove("active");
+        }
+    });
+});
